31f7534db225ac168c804fca3238f435d6537be4
Abdul Moiz <moiz@prosperdigital.com.au>
2021-04-26 14:44:43 +0500

    selected date/time format. renamed textView IDs. refactoring.

--- app/src/main/java/com/example/todonotifier/MainActivity.kt
+++ app/src/main/java/com/example/todonotifier/MainActivity.kt
@@ -7,12 +7,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var selectedYear: Int = 0
+    private var selectedMonth: Int = 0
+    private var selectedDate: Int = 0
+    private var selectedHour: Int = 0
+    private var selectedMinute: Int = 0
+    private var selectedRepeat: Int = 0
+    private var selectedStopAfter: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,48 +30,68 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress("UNUSED_PARAMETER")
     fun onClickSelectDate(view: View) {
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
+        val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
 
         val datePickerDialog = DatePickerDialog(
             this,
 //            { view, year, monthOfYear, dayOfMonth ->
             { _, year, monthOfYear, dayOfMonth ->
-                // Display Selected date in textbox
-//                lblDate.setText("" + dayOfMonth + " " + ", " + year)
+                selectedYear = year
+                selectedMonth = monthOfYear
+                selectedDate = dayOfMonth
+
+                val calendarSelected = Calendar.getInstance()
+                calendarSelected.set(year, monthOfYear, dayOfMonth)
+
+                val textViewDate: TextView = findViewById(R.id.textViewDate)
+                textViewDate.text = SimpleDateFormat(
+                    "EEE, dd MMM, yyyy",
+                    Locale.getDefault(),
+                ).format(calendarSelected.time)
             },
-            year,
-            month,
-            day
+            currentYear,
+            currentMonth,
+            currentDay
         )
+
+        // source: https://stackoverflow.com/a/33996958
+        // How to disable past dates in Android date picker?
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis() - 1000
+
         datePickerDialog.show()
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onClickSelectTime(view: View) {
-        println("aaaaaa just clicked")
-
-        val c = Calendar.getInstance()
-        val hour = c.get(Calendar.HOUR_OF_DAY)
-        val minute = c.get(Calendar.MINUTE)
+        val calendar = Calendar.getInstance()
+        val currentHour = calendar.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = calendar.get(Calendar.MINUTE)
 
         val timePickerDialog = TimePickerDialog(
             this,
 //            { view, hourOfDay, minute ->
             { _, hourOfDay, minute ->
 
-            }, hour, minute, true
-        )
+                selectedHour = hourOfDay
+                selectedMinute = minute
 
+                val textViewTime: TextView = findViewById(R.id.textViewTime)
+                textViewTime.text = ""
+                textViewTime.append("$hourOfDay:$minute")
+            },
+            currentHour,
+            currentMinute,
+            true
+        )
         timePickerDialog.show()
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onClickSelectRepeat(view: View) {
         // source: https://www.youtube.com/watch?v=Em7LJddHAbQ
-
         val data = resources.getStringArray(R.array.repeat_values)
         val listView = ListView(this)
         val builder = AlertDialog.Builder(this)
@@ -79,9 +109,9 @@ class MainActivity : AppCompatActivity() {
 
         val alertDialog = builder.create()
 
-
 //        listView.setOnItemClickListener { parent, view, position, id ->
         listView.setOnItemClickListener { _, _, position, _ ->
+            selectedRepeat = position
             Toast.makeText(this, position.toString(), Toast.LENGTH_LONG).show()
             alertDialog.dismiss()
         }
@@ -108,9 +138,9 @@ class MainActivity : AppCompatActivity() {
 
         val alertDialog = builder.create()
 
-
 //        listView.setOnItemClickListener { parent, view, position, id ->
         listView.setOnItemClickListener { _, _, position, _ ->
+            selectedStopAfter = position
             Toast.makeText(this, position.toString(), Toast.LENGTH_LONG).show()
             alertDialog.dismiss()
         }
