032d8c22fa6cad198ef449982db36fb3dad3fd1d
Abdul Moiz <moiz@prosperdigital.com.au>
2021-04-26 09:40:51 +0500

    testing

--- app/src/main/java/com/example/todonotifier/MainActivity.kt
+++ app/src/main/java/com/example/todonotifier/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("")
     }
 
     @Suppress("UNUSED_PARAMETER")
