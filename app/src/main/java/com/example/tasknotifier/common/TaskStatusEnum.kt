7b7e28b30477f6af4032cb9cf808a5277f483382
Abdul Moiz <moizhus123@gmail.com>
2021-10-09 01:40:48 +0500

    not using buggy code of getReadableStatus.

--- app/src/main/java/com/example/tasknotifier/common/TaskStatusEnum.kt
+++ app/src/main/java/com/example/tasknotifier/common/TaskStatusEnum.kt
@@ -1,7 +1,6 @@
 package com.example.tasknotifier.common
 
 import android.content.Context
-import com.example.tasknotifier.utils.MyAlarmManager
 
 enum class TaskStatusEnum {
     On,
@@ -9,13 +8,14 @@ enum class TaskStatusEnum {
 
     companion object {
         fun getReadableStatus(context: Context, id: Int, taskStatus: TaskStatusEnum, dateTime: Long): String {
-            if (MyAlarmManager.isAlarmOff(context, id)) {
-                return Off.toString()
-            }
+//            if (MyAlarmManager.isAlarmOff(context, id)) {
+//                return Off.toString()
+//            }
 //            if (taskStatus == Off) {
 //                return Off.toString()
 //            }
-            else if (dateTime < System.currentTimeMillis()) {
+//            else if (dateTime < System.currentTimeMillis()) {
+            if (dateTime < System.currentTimeMillis()) {
                 return "Expired"
             }
             return taskStatus.toString()
